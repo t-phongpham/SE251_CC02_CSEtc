e63b412fb2c760125ce2cc15aa48d74b74e315b4
d4t (c4r4m3liz3d_p0rk) <144753279+datdao2005@users.noreply.github.com>
2025-11-26 13:01:15 +0700

    add properties in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
@@ -7,6 +8,8 @@ const PORT = 3000;
 // Để đọc JSON/form data nếu cần
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 // Serve static frontend
 app.use(express.static(path.join(__dirname, '../frontend')));
@@ -22,6 +25,13 @@ app.post('/api/contact', (req, res) => {
   console.log('Contact form:', name, email, message);
   res.json({ status: 'ok', msg: 'Đã nhận form!' });
 });
+
+// Sắp xếp lịch
+//trường hợp 1: xếp lịch cho tutor
+//TODO
+
+//trường hợp 2: xếp lịch cho student
+//TODO
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
